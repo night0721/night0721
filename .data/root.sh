4d3fdfa0bf3bd0d04c65595e7662d27b8ffd1e7f
Oscar Fu <77528305+night0721@users.noreply.github.com>
2023-09-23 15:19:14 +0100

    Dot files copy

--- .data/root.sh
+++ .data/root.sh
@@ -60,6 +60,6 @@ ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 hwclock —w
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 EOCHROOT
-cp -r dotfiles/ /mnt/dotfiles/
+cp -r dotfiles/ /mnt/
 # cp dotfiles/.data/install.sh /mnt/install.sh
 arch-chroot -u night /mnt su -c /dotfiles/.data/install.sh -s /bin/sh night > /dev/null
