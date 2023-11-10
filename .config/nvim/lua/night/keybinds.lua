c5e1f539de24ef589c31ff349a14245c7bf50617
night0721 <oscarfu0721@gmail.com>
2023-11-10 22:20:15 +0000

    nvim keybind for indent

--- .config/nvim/lua/night/keybinds.lua
+++ .config/nvim/lua/night/keybinds.lua
@@ -35,6 +35,7 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+vim.keymap.set("n", "<leader>cf", "gg=G<CR>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>f", [[:%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
