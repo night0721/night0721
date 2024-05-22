853d7d683444ed5ed58baf7aaa4e828e0456a4a4
night0721 <night@night0721.xyz>
2024-05-22 01:36:44 +0100

    update nvim to use tab instead of space

--- .config/nvim/lua/night/settings.lua
+++ .config/nvim/lua/night/settings.lua
@@ -14,7 +14,7 @@ vim.opt.numberwidth = 2
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.expandtab = false 
 vim.opt.wrap = true
 vim.opt.breakindent = true
 
